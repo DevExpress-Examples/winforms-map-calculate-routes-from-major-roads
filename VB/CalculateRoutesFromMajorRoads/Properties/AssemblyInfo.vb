88a150bd8afda4bf486dfeb9064799d56cb7da87
DevExpressExampleBot <fakeEmail>
2022-08-23 10:07:06 +0400

    Source auto update [skip ci]

--- VB/CalculateRoutesFromMajorRoads/Properties/AssemblyInfo.vb
+++ VB/CalculateRoutesFromMajorRoads/Properties/AssemblyInfo.vb
@@ -1,53 +1,3 @@
-﻿' Developer Express Code Central Example:
-' How to calculate routes from major roads using the Bing Map Route web service
-' 
-' This example demonstrates how to calculate routes to the destination point from
-' major roads using the BingRouteDataProvider.CalculateRoutesFromMajorRoads
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteDataProvider_CalculateRoutesFromMajorRoadstopic)
-' method.
-' Before route calculation, specify destination point coordinates
-' (GeoPoint.Latitude
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapGeoPoint_Latitudetopic)
-' and GeoPoint.Longitude
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapGeoPoint_Longitudetopic)).
-' In addition, you can specify optional parameters: the destination name, driving
-' or walking route travel mode using the BingRouteOptions.Mode
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteOptions_Modetopic)
-' property and route optimization options to calculate an optimal route either by
-' time or by distance via the BingRouteOptions.RouteOptimization
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteOptions_RouteOptimizationtopic)
-' property.
-' 
-' To start the application, click the Calculate Routes From Major
-' Roads button. All parameters are passed to the CalculateMajorRouteRequest
-' method, and you can see the results in the rich text box element and calculated
-' routes on a map. The requested results contain the total distance of a route,
-' itinerary item (BingRouteResult.Distance
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteResult_Distancetopic),
-' BingRouteLeg.Distance
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteLeg_Distancetopic),
-' BingItineraryItem.Distance
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingItineraryItem_Distancetopic)),
-' the time required to follow the calculated route (BingRouteResult.Time
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteResult_Timetopic))
-' and pass the rout leg and itinerary item (BingRouteLeg.Time
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteLeg_Timetopic),
-' BingItineraryItem.Time
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingItineraryItem_Timetopic)).
-' You can also see the maneuvers associated with the itinerary item
-' (BingItineraryItem.Maneuver
-' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingItineraryItem_Maneuvertopic))
-' and other parameters.
-' Note that if you run this sample as is, you will get a
-' warning message informing that the specified Bing Maps key is invalid. To learn
-' how to register a Bing Maps account and create a key for it, refer to the How
-' to: Get a Bing Maps Key
-' (http://documentation.devexpress.com/#WindowsForms/CustomDocument15102)
-' tutorial.
-' 
-' You can find sample updates and versions for different programming languages here:
-' http://www.devexpress.com/example=E5064
-
 ' Developer Express Code Central Example:
 ' How to calculate routes from major roads using the Bing Map Route web service
 ' 
@@ -97,7 +47,55 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5064
-
+' Developer Express Code Central Example:
+' How to calculate routes from major roads using the Bing Map Route web service
+' 
+' This example demonstrates how to calculate routes to the destination point from
+' major roads using the BingRouteDataProvider.CalculateRoutesFromMajorRoads
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteDataProvider_CalculateRoutesFromMajorRoadstopic)
+' method.
+' Before route calculation, specify destination point coordinates
+' (GeoPoint.Latitude
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapGeoPoint_Latitudetopic)
+' and GeoPoint.Longitude
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapGeoPoint_Longitudetopic)).
+' In addition, you can specify optional parameters: the destination name, driving
+' or walking route travel mode using the BingRouteOptions.Mode
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteOptions_Modetopic)
+' property and route optimization options to calculate an optimal route either by
+' time or by distance via the BingRouteOptions.RouteOptimization
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteOptions_RouteOptimizationtopic)
+' property.
+' 
+' To start the application, click the Calculate Routes From Major
+' Roads button. All parameters are passed to the CalculateMajorRouteRequest
+' method, and you can see the results in the rich text box element and calculated
+' routes on a map. The requested results contain the total distance of a route,
+' itinerary item (BingRouteResult.Distance
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteResult_Distancetopic),
+' BingRouteLeg.Distance
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteLeg_Distancetopic),
+' BingItineraryItem.Distance
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingItineraryItem_Distancetopic)),
+' the time required to follow the calculated route (BingRouteResult.Time
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteResult_Timetopic))
+' and pass the rout leg and itinerary item (BingRouteLeg.Time
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteLeg_Timetopic),
+' BingItineraryItem.Time
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingItineraryItem_Timetopic)).
+' You can also see the maneuvers associated with the itinerary item
+' (BingItineraryItem.Maneuver
+' (http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingItineraryItem_Maneuvertopic))
+' and other parameters.
+' Note that if you run this sample as is, you will get a
+' warning message informing that the specified Bing Maps key is invalid. To learn
+' how to register a Bing Maps account and create a key for it, refer to the How
+' to: Get a Bing Maps Key
+' (http://documentation.devexpress.com/#WindowsForms/CustomDocument15102)
+' tutorial.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E5064
 ' Developer Express Code Central Example:
 ' How to get routes to the destination point using the Bing Map Route data service
 ' 
@@ -148,65 +146,35 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E5064
+Imports System.Reflection
+Imports System.Runtime.InteropServices
 
-'------------------------------------------------------------------------------
-' <auto-generated>
-'     This code was generated by a tool.
-'     Runtime Version:4.0.30319.18408
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly:AssemblyTitle("CalculateRoutesFromMajorRoads")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("CalculateRoutesFromMajorRoads")>
+<Assembly:AssemblyCopyright("Copyright ©  2014")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly:ComVisible(False)>
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly:Guid("3db906fb-3d3e-46f2-9f79-bf7824e2b22c")>
+' Version information for an assembly consists of the following four values:
 '
-'     Changes to this file may cause incorrect behavior and will be lost if
-'     the code is regenerated.
-' </auto-generated>
-'------------------------------------------------------------------------------
-
-Namespace My.Resources
-
-
-    ''' <summary>
-    '''   A strongly-typed resource class, for looking up localized strings, etc.
-    ''' </summary>
-    ' This class was auto-generated by the StronglyTypedResourceBuilder
-    ' class via a tool like ResGen or Visual Studio.
-    ' To add or remove a member, edit your .ResX file then rerun ResGen
-    ' with the /str option, or rebuild your VS project.
-    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0"), Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(), _
-    Global.Microsoft.VisualBasic.HideModuleNameAttribute()> _
-    Friend Module Resources
-
-        Private resourceMan As Global.System.Resources.ResourceManager
-
-        Private resourceCulture As Global.System.Globalization.CultureInfo
-
-'        internal Resources()
-'        {
-'        }
-
-        ''' <summary>
-        '''   Returns the cached ResourceManager instance used by this class.
-        ''' </summary>
-        <Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
-        Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
-            Get
-                If (resourceMan Is Nothing) Then
-                    Dim temp As New Global.System.Resources.ResourceManager("Resources", GetType(Resources).Assembly)
-                    resourceMan = temp
-                End If
-                Return resourceMan
-            End Get
-        End Property
-
-        ''' <summary>
-        '''   Overrides the current thread's CurrentUICulture property for all
-        '''   resource lookups using this strongly typed resource class.
-        ''' </summary>
-        <Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
-        Friend Property Culture() As Global.System.Globalization.CultureInfo
-            Get
-                Return resourceCulture
-            End Get
-            Set(ByVal value As System.Globalization.CultureInfo)
-                resourceCulture = value
-            End Set
-        End Property
-    End Module
-End Namespace
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' [assembly: AssemblyVersion("1.0.*")]
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
